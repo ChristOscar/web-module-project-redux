6ec1ce977e0c9e63e4cd9c408690d3dff17ab5a0
Christwide Oscar <triple101.games@gmail.com>
2022-01-18 18:50:09 -0600

    Finished the MVP

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 
@@ -22,4 +22,4 @@ const FavoriteMovieList = (props) => {
 }
 
 
-export default FavoriteMovieList;
+export default connect(mapStateToProps)(FavoriteMovieList);
--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
-);
+const store = createStore(reducer);
+
+ ReactDOM.render(
+   <Provider store={store}>
+     <Router>
+       <App />
+     </Router>
+   </Provider>,
+   document.getElementById('root')
+ );
